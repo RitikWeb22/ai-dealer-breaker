63874570cf6096bb30ac20beffef8ef3dc401ca5
Ritik <ritikweb30>
2026-03-28 17:13:59 +0530

    done

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use("/api/auth", authRouter)
 
 // agent negotiation routes
-app.use("/api/vapi", NegotiationRouter)
+app.use("/api/negotiation", NegotiationRouter)
 
 // product routes
 app.use("/api/product", productRouter)
